6e816aacafdd547faed45783e49780d940ff7c21
alabulei1 <45785633+alabulei1@users.noreply.github.com>
2021-03-02 15:12:10 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,46 +5,45 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Learn WebAssembly</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://webassemblytoday.substack.com/">WebAssembly!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by <a href="https://github.com/second-state/wasm-learning">Wasm-learning</a>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://webassemblytoday.substack.com/" className={styles.card}>
+            <h3>Newsletter;</h3>
+            <p>Find something new in WebAssembly community.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="https://www.secondstate.io/articles/getting-started-with-function-as-a-service-in-rust/" className={styles.card}>
             <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <p>Learn about Rust and WebAssembly on the server-side!</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="https://second-state.github.io/wasm-learning/faas/mobilenet/html/index.html"
             className={styles.card}
           >
             <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <p>Discover and deploy TensorFlow as a service examples with Rust and WebAssembly.</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Low code Rust &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              create modern web applications using only simple Rust syntax.
             </p>
           </a>
         </div>
@@ -52,7 +51,7 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.secondstate.io/faas/"
           target="_blank"
           rel="noopener noreferrer"
         >
